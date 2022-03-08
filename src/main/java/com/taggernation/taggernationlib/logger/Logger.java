a3325fa60bc8c61c60b08df2852f10d78d63e91d
ToonyStank <hakeemiqbalnew@gmail.com>
2022-03-08 23:26:51 +0530

    Logger update

--- src/main/java/com/taggernation/taggernationlib/logger/Logger.java
+++ src/main/java/com/taggernation/taggernationlib/logger/Logger.java
@@ -11,13 +11,19 @@ import static com.taggernation.taggernationlib.TaggerNationLib.*;
 public class Logger {
     String pluginName = adventureFormat("<gradient:#cf1a91:#fbdbf4>[ </gradient>" + " <white><bold>" + "<pluginName>" + " </white></bold>" + "<gradient:#cf1a91:#fbdbf4> ]</gradient>  <red>➜  </red>");
 
+    Plugin plugin;
+
+    public Logger(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+
     /**
      * Send info message to console
      * @param message Message to send
-     * @param plugin Plugin to send message from
      * @param adventureFormat Whether to use adventure format
      */
-    public void info(String message, Plugin plugin, boolean adventureFormat) {
+    public void info(String message, boolean adventureFormat) {
         if (!adventureFormat) {
             Bukkit.getLogger().info(pluginName.replace("<pluginName>", plugin.getName()) + ChatColor.translateAlternateColorCodes('&', message));
         }
@@ -27,10 +33,9 @@ public class Logger {
     /**
      * Send warning message to console
      * @param message Message to send
-     * @param plugin Plugin to send message from
      * @param adventureFormat Whether to use adventure format
      */
-    public void warn(String message, Plugin plugin, boolean adventureFormat) {
+    public void warn(String message, boolean adventureFormat) {
         if (!adventureFormat) {
             Bukkit.getLogger().info(pluginName.replace("<pluginName>", plugin.getName()) + ChatColor.YELLOW + ChatColor.translateAlternateColorCodes('&', message));
         }
@@ -40,10 +45,9 @@ public class Logger {
     /**
      * Send error message to console
      * @param message Message to send
-     * @param plugin Plugin to send message from
      * @param adventureFormat Whether to use adventure format
      */
-    public void error(String message, Plugin plugin, boolean adventureFormat) {
+    public void error(String message, boolean adventureFormat) {
         if (!adventureFormat) {
             Bukkit.getLogger().info(pluginName.replace("<pluginName>", plugin.getName()) + ChatColor.RED + ChatColor.translateAlternateColorCodes('&', message));
         }
