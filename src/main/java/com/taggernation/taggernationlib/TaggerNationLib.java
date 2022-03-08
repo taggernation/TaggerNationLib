212d36211596b59e07a080d14c99b56de79df40b
ToonyStank <hakeemiqbalnew@gmail.com>
2022-03-08 23:20:29 +0530

    MiniMessage

--- src/main/java/com/taggernation/taggernationlib/TaggerNationLib.java
+++ src/main/java/com/taggernation/taggernationlib/TaggerNationLib.java
@@ -1,7 +1,10 @@
 package com.taggernation.taggernationlib;
 
 import com.taggernation.taggernationlib.placeholder.Placeholder;
+import io.github.alen_alex.messageframework.MessageFramework;
+import io.github.alen_alex.messageframework.bukkit.framework.FrameworkBuilder;
 import lombok.Getter;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -11,6 +14,8 @@ public class TaggerNationLib extends JavaPlugin {
 
     public static TaggerNationLib plugin;
     public static Placeholder papiHook;
+    public static MessageFramework messageFramework;
+    public static MiniMessage miniMessage;
 
     public void papiExist() {
         if (plugin.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
@@ -24,6 +29,8 @@ public class TaggerNationLib extends JavaPlugin {
         // Plugin startup logic
         plugin = this;
         papiHook = new Placeholder();
+        messageFramework = new FrameworkBuilder().setPlugin(this).withMiniMessageEngine().build();
+        miniMessage = MiniMessage.builder().build();
 
         papiExist();
     }
