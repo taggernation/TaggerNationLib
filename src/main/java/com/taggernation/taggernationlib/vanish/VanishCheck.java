f0707aab05285366c3abc2e6a574a717c88ac9ff
RONiN <69498033+RealRONiN@users.noreply.github.com>
2022-03-10 14:54:26 +0530

    ` + ` VanishHook.

---
+++ src/main/java/com/taggernation/taggernationlib/vanish/VanishCheck.java
@@ -0,0 +1,16 @@
+package com.taggernation.taggernationlib.vanish;
+
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
+
+public class VanishCheck {
+  /**
+   *@param player Player to check if they're vanished or not.
+   **/
+  public boolean isVanished(Player player) {
+    for (final MetadataValue meta : player.getMetadata("vanished")) {
+      if (meta.asBoolean()) return true;
+    }
+    return false;
+  }
+}
