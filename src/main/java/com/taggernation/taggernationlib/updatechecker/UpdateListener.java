64a95f87741841618cea754de2cbcfc4b28185f7
RONiN <69498033+RealRONiN@users.noreply.github.com>
2022-05-19 10:55:46 +0530

    ` + ` Made changes with SpigotMessenger

--- src/main/java/com/taggernation/taggernationlib/updatechecker/UpdateListener.java
+++ src/main/java/com/taggernation/taggernationlib/updatechecker/UpdateListener.java
@@ -5,21 +5,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import static com.taggernation.taggernationlib.TaggerNationLib.messageFramework;
+import static com.taggernation.taggernationlib.TaggerNationLib.messenger;
 
 public class UpdateListener implements Listener {
 
-    private final UpdateChecker instance;
+  private final UpdateChecker instance;
 
-    public UpdateListener(UpdateChecker instance) {
-        this.instance = instance;
-    }
+  public UpdateListener(UpdateChecker instance) {
+    this.instance = instance;
+  }
 
-    @EventHandler
-    public void playerJoinNotification(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        if ((player.isOp() && instance.isOpNotificationEnabled()) || (instance.getNotificationPermission() != null && player.hasPermission(instance.getNotificationPermission()))) {
-            messageFramework.sendMessage(player,(instance.getMessage()));
-        }
+  @EventHandler
+  public void playerJoinNotification(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    if ((player.isOp() && instance.isOpNotificationEnabled()) ||
+            (instance.getNotificationPermission() != null &&
+                    player.hasPermission(instance.getNotificationPermission()))) {
+      for (final String message : instance.getMessage()) {
+        messenger.sendMessage(player, message);
+      }
     }
+  }
 }
